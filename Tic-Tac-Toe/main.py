ac76f884fa3d756f0b115f5f844b411c1e44fbd6
Ahmed Hesham <ahmedhesham122000@gmail.com>
2026-03-12 10:44:34 +0200

    add icon for the game

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -1,7 +1,10 @@
 import tkinter as tk
+from tkinter import ttk
 from TicTacToe import TicTacToe
 
 if __name__ == "__main__":
     root = tk.Tk()
+    icon = tk.PhotoImage(file="./.screenshots/tictactoe_icon.png")
     TicTacToe(root)
+    root.iconphoto(False, icon)
     root.mainloop()
